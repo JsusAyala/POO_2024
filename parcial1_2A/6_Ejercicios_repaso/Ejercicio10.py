1d8488ab20fa1fc0dcf47504a0ce3a97faa5b195
JsusAyala <jesusalfonsoayalaayala@gmail.com>
2024-05-23 09:37:49 -0600

    confirmar

--- parcial1_2A/6_Ejercicios_repaso/Ejercicio10.py
+++ parcial1_2A/6_Ejercicios_repaso/Ejercicio10.py
@@ -1,12 +1,21 @@
 #Crear un programa que solicite la calificacion de 15 alumnos e imprimir en pantalla cuantos aproboron y cuantos no aprobaron
 
 contador=1
+reprobados=0
+aprobados=0
+error=0
 while contador<=15:
     calif = float(input(f"Ingresar la calificacion del alumno {contador}: "))
     contador+=1
     if calif <= 7.9:
         print("Reprobo")
-    else:
+        reprobados+=1
+    if calif >= 8 and calif <= 10:
         print("Aprobo")
+        aprobados+=1
     if calif >= 10.1:
         print("Calificacion invalida")
+        error+=1
+print(f"Los estudiantes que aprobaron fueron {aprobados} y los estudiantes que reprobaron son {reprobados}.")
+if error >= 1:
+   print(f"Hay un total de {error} calificaciones invalidas favor de verificar en rectoria.")
